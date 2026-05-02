9f29b163e967c8b452e077d916fd7b3d9719a832
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2026-05-02 09:49:15 +0530

    Add pattern printing program in dsa94.java
    Implement a pattern printing program that displays a pyramid of stars.

---
+++ Pattern Printing/dsa94.java
@@ -0,0 +1,32 @@
+/*
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+
+*/
+public class dsa94 {
+  public static void main(String args[])
+    {
+      int n=5;
+      //row 
+      for(int row=1;row<=n;row++)
+      {
+        //space
+        for(int col=1;col<=n-row;col++)
+        {
+          System.out.print("  ");
+        }
+        //star
+        for(int col=1;col<=2*row-1;col++)
+        {
+          System.out.print("* ");
+        }
+
+        //next
+        System.out.println();
+      }
+    }
+  
+}
